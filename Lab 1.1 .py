8f809aef2d3901d07da090c3ff602f7a3a558457
ChianSeong04 <90165922+ChianSeong04@users.noreply.github.com>
2021-09-06 10:41:33 +0800

    Add files via upload

---
+++ Lab 1.1 .py
@@ -0,0 +1,12 @@
+# Student ID: 1201200637 #
+# Student Name: Shawn #
+
+ 
+person=input("Please Enter you name:")
+print("Hello",person)
+
+#Lab 1.2 get input for two number and diaplay the sum of the numbers
+num1=input("Enter Num1:")
+num2=input("Enter Num2:")
+sum=num1+num2
+print("Sum is",sum)
---
+++ Lab 1.2.py
@@ -0,0 +1,7 @@
+#Lab 1.2 get input for two number and diaplay the sum of the numbers1
+num1=int(input("Enter Num1:"))
+num2=int(input("Enter Num2:"))
+
+sum=num1+num2
+print("Sum is",sum)
+print("The Sum is ",sum,"",num1,"and",num2)
