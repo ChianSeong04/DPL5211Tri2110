64b307f12541b2d6312e36fbdb6a4122a16b19cf
ChianSeong04 <90165922+ChianSeong04@users.noreply.github.com>
2021-09-13 09:41:16 +0800

    Add files via upload

---
+++ lab 2 q1.py
@@ -0,0 +1,6 @@
+#Student ID:1201200637
+# Student Name:Wee Chian Seong
+
+kg= float(input("Please Enter the value of Koligram:"))
+gram= kg*1000
+print("For {:.2f} Kilogram is equivalent to {:.2f} gram".format(kg,gram))
---
+++ lab 2 q2.py
@@ -0,0 +1,13 @@
+#Student ID:1201200637
+#Student Name:Wee Chian Seong
+import math
+
+FT=0.3048 #constant does not change in capital letter
+
+feet=float(input("Enter the value of feet:"))
+meter=feet*FT
+print("Meter is {:.2f} Feet is {:.2f}".format(meter,feet))
+
+print(math.pi)
+print(math.e)
+print(math.)
